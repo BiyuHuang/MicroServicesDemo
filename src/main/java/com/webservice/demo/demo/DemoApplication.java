b3248639a94d92da8c5d46f9eff0f25216d383b0
Wallace Huang <h417652303@gmail.com>
2023-12-04 18:56:49 +0800

    [experimental]try to add gateway

--- src/main/java/com/webservice/demo/demo/DemoApplication.java
+++ src/main/java/com/webservice/demo/demo/DemoApplication.java
@@ -2,12 +2,12 @@ package com.webservice.demo.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+@EnableDiscoveryClient
 @SpringBootApplication
 public class DemoApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
-	}
-
+  public static void main(String[] args) {
+    SpringApplication.run(DemoApplication.class, args);
+  }
 }
